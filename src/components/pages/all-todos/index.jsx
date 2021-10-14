f2e2984eae57fd0e844ab4bd522ce04e8efaa3f5
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2021-10-14 19:36:37 -0400

    Added form for new todos
    Tutorial 5 submission

--- src/components/pages/all-todos/index.jsx
+++ src/components/pages/all-todos/index.jsx
@@ -2,21 +2,16 @@ import "./styles.css";
 import { Todo } from "../../todo/index";
 
 export const AllTodosPage = () => {
-    return(
-        <div>
-            <Todo
-                text="Walk my dog" date="10/10/2021" color="blue"
-            >
-            </Todo>
-            <Todo
-                text="Buy fabric softener" date="10/10/2021" color="blue"
-            >
-            </Todo>
-            <Todo
-                text="Book train to Toronto" date="10/10/2021" color="blue"
-            >
-            </Todo>
+    const todos = [
+        {text: "Walk my dog", date: "10/10/2021", color: "blue", isComplete: false},
+        {text: "Buy fabric softener", date: "10/10/2021", color: "red", isComplete: false},
+        {text: "Do assignment", date: "10/10/2021", color: "red", isComplete: false},
+    ]
 
+    // Cycle through the todos array and display all of them
+    return(
+        <div className="todos-container">
+            { todos.map((todo) => <Todo text={todo.text} date={todo.date} color={todo.color} isComplete={todo.isComplete} />) }
         </div>
     )
 }
